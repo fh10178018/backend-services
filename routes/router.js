f20afd9b4a9d0e0a16ae8eea0570d604bba212a4
fh10178018 <1206758827@qq.com>
2021-01-04 22:14:32 +0800

    :new: create data initialization interface

--- routes/router.js
+++ routes/router.js
@@ -2,10 +2,12 @@ const { usersRouter } = require('./users');
 const resumesRouter = require('./resumes');
 const { fileRouter } = require('./file');
 const blogRouter = require('./blog');
+const initializationRouter = require('./initialization');
 module.exports = app => {
   app.use('/user', usersRouter)
   app.use('/resumes', resumesRouter)
   app.use('/imagefile', fileRouter)
   app.use('/blog', blogRouter)
+  app.use('/initialization', initializationRouter)
 };
 
